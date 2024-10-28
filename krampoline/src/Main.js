eb6f31695bb7b1f99a4a02fb65863b3cbba6d77a
junest66 <chlwndks333@gmail.com>
2024-10-28 22:35:47 +0900

    feat: ai 버튼 추가

--- krampoline/src/Main.js
+++ krampoline/src/Main.js
@@ -3,41 +3,56 @@ import './App.css';
 import { instance } from "./api.js";
 
 function Main() {
-    
   const [apiTest, setApiTest] = useState(false);
   const [dbTest, setDbTest] = useState(false);
-    
-  const handleApiOnclick = async () => {
-      try {
-          await instance.get("/test");
-          setApiTest(true);
-      } catch(err) {
-          console.log(err);
-      }
-      
-  }
-  
+  const [aiResponse, setAiResponse] = useState("");
+
+  const handleApiOnClick = async () => {
+    try {
+      await instance.get("/test");
+      setApiTest(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const handleDbOnClick = async () => {
-      try {
-          await instance.get("/db");
-          setDbTest(true);
-      } catch(err) {
-          console.log(err);
-      }
-  }
-  
+    try {
+      await instance.get("/db");
+      setDbTest(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAiGenerate = async () => {
+    try {
+      const response = await instance.get("/ai/generate", {
+        params: { message: "Tell me a joke" },
+      });
+      setAiResponse(response.data.generation);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        
         <div>
-          Hello Krampoline !
+          Hello Krampoline!
         </div>
         <div>
-          <button onClick={handleApiOnclick}>API TEST</button><div>{apiTest?"CONNECT":"NOT YET"}</div>
+          <button onClick={handleApiOnClick}>API TEST</button>
+          <div>{apiTest ? "CONNECT" : "NOT YET"}</div>
         </div>
-          <div>
-          <button onClick={handleDbOnClick}>DB TEST</button><div>{dbTest?"CONNECT":"NOT YET"}</div>
+        <div>
+          <button onClick={handleDbOnClick}>DB TEST</button>
+          <div>{dbTest ? "CONNECT" : "NOT YET"}</div>
+        </div>
+        <div>
+          <button onClick={handleAiGenerate}>AI Generate</button>
+          <div>{aiResponse || "Waiting for response..."}</div>
         </div>
       </header>
     </div>
